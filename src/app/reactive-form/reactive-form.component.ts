e354b575f8358bc554c973a397a207f7d58c1546
rLaurentePn2k19 <rangie.laurente@student.passerellesnumeriques.org>
2020-01-20 16:46:22 +0800

    initial

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
 @Component({
@@ -11,27 +11,31 @@ export class ReactiveFormComponent implements OnInit {
 
   public dangerColor = "red"
   public successColor = "green"
+  userInformation : FormGroup
 
   show = false;
-
+  
   constructor(private fb: FormBuilder) { }
 
-  userInformation = this.fb.group({
-    firstname: ['', Validators.required],
-    lastname: ['', Validators.required],
-    email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")]],
-    address: this.fb.group({
-      street: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      zipcode: ['', Validators.required],
-    })
-  })
 
   ngOnInit() {
+    this.userInformation = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: 
+      ['', 
+        [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")]
+      ],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        zipcode: ['', Validators.required],
+      })
+    })
   }
 
-  onSubmit() {
+  onSubmit() {  
     this.show = !this.show;
   }
 
